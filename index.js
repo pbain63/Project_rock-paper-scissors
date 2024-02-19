e0cee4dd36e6b0904a531db78decf5273ee31f6a
pbain63 <prodipbain63@gmail.com>
2024-02-19 15:16:04 +0600

    'create new function called playGame() in index.js' "Use playRound(playerSelection, computerSelection) inside of this one to play a five round game that keeps score and reports a winner or loser at the end."

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-//algorithm:
-//1.3.create function that'll return random either ‘Rock’,
-//‘Paper’ or ‘Scissors’.
-//2.4. write a function take two parameters - the
-//playerSelection and computerSelection - and then
-//return a string that declares the winner or tie
-//of the round like so: "You Lose! Paper beats Rock"
-//3. Takes random user choice as "prompt()" and play 
-//with computer infinitely
-//
-//
 
 arrayOfChoices = ["Rock", "Paper", "Scissors"];
-
-const userChoice = prompt(
-  "Type any word from the 'Rock', 'Paper', 'Scissors' to play: "
-);
-
-const playerSelection =
-  userChoice[0].toUpperCase() + userChoice.slice(1).toLowerCase();
-
-console.log("Player selected: " + playerSelection);
-
+//1.3.create function that'll return random either ‘Rock’,
+//‘Paper’ or ‘Scissors’.
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3);
 
@@ -31,11 +12,20 @@ function getComputerChoice() {
 getComputerChoice();
 //console.log(getComputerChoice()); /*return output but not same
 
+let userScore = parseInt(0);
+let computerScore = parseInt(0);
+
+//2.4. write a function take two parameters - the
+//playerSelection and computerSelection - and then
+//return a string that declares the winner or tie
+//of the round like so: "You Lose! Paper beats Rock"
 function playRound(playerSelection, computerSelection) {
   //let playerSelection=ROCK
   if (playerSelection === "Rock" && computerSelection === "Paper") {
+    computerScore++;
     return "You Lose! Paper beats Rock";
   } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    userScore++;
     return "You Win! Rock beats Scissors";
   } else if (playerSelection === "Rock" && computerSelection === "Rock") {
     return "It's a Draw! Let's play again";
@@ -44,17 +34,17 @@ function playRound(playerSelection, computerSelection) {
   //let playerSelection=Paper
   else if (playerSelection === "Paper" && computerSelection === "Paper") {
     return "It's a Draw! Let's play again";
-  } else if (
-    playerSelection === "Paper" &&
-    computerSelection === "Scissors"
-  ) {
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    computerScore++;
     return "You Lose! Scissors beat Paper";
   } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    userScore++;
     return "You Win! Rock beats Paper";
   }
 
   //let playerSelection=Scissors
   else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    userScore++;
     return "You Win! Scissors beat Paper";
   } else if (
     playerSelection === "Scissors" &&
@@ -62,22 +52,36 @@ function playRound(playerSelection, computerSelection) {
   ) {
     return "It's a Draw! Let's play again";
   } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    computerScore++;
     return "You Lose! Rock beats Scissors";
   }
   return;
 }
 
-//const userChoice = "rock";
-
-const computerSelection = getComputerChoice();
-console.log("Computer selection: " + computerSelection);
-const roundPlay = playRound(playerSelection, computerSelection);
-console.log("Play round result: " + roundPlay);
-
-
+//3.6. create new function called playGame(). Use the
+//previous function inside of this one to play a five
+//round game that keeps score and reports a winner or
+//loser at the end.
 function playGame() {
-  
+  for (let i = 0; i < 5; i++) {
+    const userChoice = prompt(
+      "Type any word from the 'Rock', 'Paper', 'Scissors' to play: "
+    );
+
+    const playerSelection =
+      userChoice[0].toUpperCase() + userChoice.slice(1).toLowerCase();
+    console.log("You selected: " + playerSelection);
+
+    const computerSelection = getComputerChoice();
+    console.log("Computer selected: " + computerSelection);
+
+    console.log(
+      "Play round result = " + playRound(playerSelection, computerSelection)
+    );
+    console.log("After round " + i + ". Your Score = " + userScore);
+    console.log("After round " + i + ". Computer's Score = " + computerScore);
+  }
   return;
 }
-playGame();
 
+playGame();
